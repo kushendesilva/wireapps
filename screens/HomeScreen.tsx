02428a05b97d0162b6900c6e9a9dceaac9423d19
Kushen De Silva <kushendesilva@gmail.com>
2024-07-19 03:31:42 +0530

    tailwind setup

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 interface HomeScreenProps {}
 
 const HomeScreen: React.FC<HomeScreenProps> = () => {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Home Screen</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
